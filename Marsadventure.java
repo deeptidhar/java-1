6e96d971331ca7984879ad8802ee79f33548cb21
Deepti Dhar <deeptidhar2007@gmail.com>
2021-06-01 12:43:11 +0000

    finished

---
+++ Marsadventure.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Marsadventure {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("What is your name ?");
+
+    String color = input.nextLine();  // Get user input
+    System.out.println("Nice, to meet you ");
+  
+
+
+    
+    System.out.println("What color will their spacesuit be? ");
+    String spacesuit = input.nextLine();
+    System.out.println("What is their pet's Name?");
+    String pet =input.nextLine();
+
+  }
+}   
